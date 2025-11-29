7d224817863fb9365f22b3d6c2760f2f6062b1a9
alodevloper <alodevloper@gmail.com>
2025-11-29 18:43:10 +0700

    uncommint

--- android/src/main/kotlin/com/nyartech/hyperpay/HyperpayPlugin.kt
+++ android/src/main/kotlin/com/nyartech/hyperpay/HyperpayPlugin.kt
@@ -177,7 +177,7 @@ class HyperpayPlugin : FlutterPlugin, MethodCallHandler, ITransactionListener, A
                     }
 
                     // Use the safe 'activity' variable
-                    paymentProvider = OppPaymentProvider(activity.application, providerMode,Connect.Region.EU);
+                    paymentProvider = OppPaymentProvider(activity.application, providerMode);
 
                     // Set the 3DS listener
                     paymentProvider!!.setThreeDSWorkflowListener{activity}
